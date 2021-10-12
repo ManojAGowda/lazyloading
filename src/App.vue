7e81d6ed1937b1a9c9103f3f3a99f2d691b2d398
ManojAGowda <90241350+ManojAGowda@users.noreply.github.com>
2021-10-12 10:28:15 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+  <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/home/Home.vue';
+export default {
+  name: "App",
+  data() {
+    return {
+      checkedData: [],
+    };
+  },
+  components:{
+    Home
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
